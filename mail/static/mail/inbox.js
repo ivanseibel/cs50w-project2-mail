cee914366946dd815ba117396b7589ec1d08c92f
Ivan L. Seibel <me@ivanseibel.dev>
2021-05-30 15:18:36 -0300

    feat: implement Mailbox - Make request to get emails - Render emails on the screen - If email is unread, apply gray background, if is read apply white background.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -36,6 +36,9 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Get emails
+  get_emails(mailbox);
 }
 
 function send_email() {
@@ -70,5 +73,60 @@ function send_email() {
       console.log(error)
       status.innerHTML = error;
       status.style.color = "red";
+    });
+}
+
+function get_emails(mailbox) {
+  // Get params
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  const status = document.querySelector('#status');
+
+  // Get emails from an specific mailbox
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      // List all emails
+      const emails_view = document.querySelector('#emails-view');
+      emails.forEach(email => {
+        const row_element = document.createElement("div");
+        const sender_element = document.createElement("div");
+        const subject_element = document.createElement("div");
+        const timestamp_element = document.createElement("div");
+
+        row_element.className = "mail-row";
+
+        if (email.read === true) {
+          row_element.className = row_element.className + " read";
+        }
+
+        sender_element.className = "col-2 sender"
+        subject_element.className = "col-7"
+        timestamp_element.className = "col-3"
+
+        sender_element.innerHTML = email.sender;
+        subject_element.innerHTML = email.subject;
+        console.log(email.timestamp);
+        date = new Date(email.timestamp);
+        console.log(date);
+        timestamp_element.innerHTML = date.toLocaleString('en-US', {
+          month: 'long',
+          day: 'numeric',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+        });
+
+
+        row_element.appendChild(sender_element);
+        row_element.appendChild(subject_element);
+        row_element.appendChild(timestamp_element);
+
+        emails_view.appendChild(row_element);
+      });
     });
 }
